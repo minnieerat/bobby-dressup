19adaf678b57c7348213e53a973f7c52e4bef1a4
Xinyue <ma_xinyue@students.edu.sg>
2025-08-02 10:16:28 +0800

    Flip flop

--- bobby dressup/ContentView.swift
+++ bobby dressup/ContentView.swift
@@ -21,6 +21,7 @@ struct ContentView: View {
             
             EmojiView(symbol: "🩴", size: 50, x: 45,  y: 70)
             EmojiView(symbol: "", size: 0, x: 0,  y: 0)
+            EmojiView(symbol: "🩴", size: 50, x: -38,  y: 70)
             EmojiView(symbol: "", size: 0, x: 0,  y: 0)
         }
         .frame(width: 200, height: 320)
