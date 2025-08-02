d6266c0f16d54c84465933a3797cd28bdbeca370
minyii <minniieeerat@gmail.com>
2025-08-02 10:41:27 +0800

    stickmanview

--- bobby dressup/StickMan.swift
+++ bobby dressup/StickMan.swift
@@ -1,39 +1,28 @@
 //
-//  ContentView.swift
+//  StickMan.swift
 //  bobby dressup
 //
 //  Created by minyi on 2/8/25.
-
+//
 import SwiftUI
 
-struct ContentView: View {
+/// Basic stickman
+struct StickManView: View {
     var body: some View {
         ZStack {
-            StickMan()
+            StickMan2()
                 .stroke(lineWidth: 4)
                 .foregroundColor(.black)
-
-            // Add more EmojiViews to decorate your Bobby
-            EmojiView(symbol: "😼", size: 85, x: 0,  y: -130)
-            EmojiView(symbol: "🍔", size: 70, x: 0,  y: -180)
-            EmojiView(symbol: "👀", size: 45, x: 0,  y: -190)
-            EmojiView(symbol: "👘", size: 116, x: 0,  y: -35)
             
-            EmojiView(symbol: "🩴", size: 50, x: 45,  y: 70)
-            EmojiView(symbol: "", size: 0, x: 0,  y: 0)
-            EmojiView(symbol: "🩴", size: 50, x: -38,  y: 70)
-            EmojiView(symbol: "", size: 0, x: 0,  y: 0)
-            EmojiView(symbol: "✌️", size: 50, x: -69,  y: -65)
         }
-        .frame(width: 200, height: 320)
     }
 }
 
-/// Basic stickman
-struct StickMan: Shape {
+struct StickMan2: Shape {
+    
     func path(in rect: CGRect) -> Path {
         var p = Path()
-
+        
         // Head
         let headRadius: CGFloat = 30
         let headCentre = CGPoint(x: rect.midX, y: rect.minY + headRadius)
@@ -42,12 +31,12 @@ struct StickMan: Shape {
             y: headCentre.y - headRadius,
             width: headRadius * 2,
             height: headRadius * 2))
-
+        
         // Body line
         let neck = CGPoint(x: headCentre.x, y: headCentre.y + headRadius)
         let hip = CGPoint(x: headCentre.x, y: neck.y + 90)
         p.move(to: neck); p.addLine(to: hip)
-
+        
         // Arms
         let leftHand  = CGPoint(x: neck.x - 60, y: neck.y + 40)
         let rightHand = CGPoint(x: neck.x + 60, y: neck.y + 40)
@@ -55,30 +44,19 @@ struct StickMan: Shape {
         p.addLine(to: leftHand)
         p.move(to: CGPoint(x: neck.x, y: neck.y + 30))
         p.addLine(to: rightHand)
-
+        
         // Legs
         let leftFoot  = CGPoint(x: hip.x - 45, y: hip.y + 90)
         let rightFoot = CGPoint(x: hip.x + 45, y: hip.y + 90)
         p.move(to: hip); p.addLine(to: leftFoot)
         p.move(to: hip); p.addLine(to: rightFoot)
-
+       
         return p
     }
 }
 
-/// Reusable emoji decorator
-struct EmojiView: View {
-    let symbol: String
-    let size: CGFloat
-    let x: CGFloat
-    let y: CGFloat
-    var body: some View {
-        Text(symbol)
-            .font(.system(size: size))
-            .offset(x: x, y: y)
-    }
-}
 
 #Preview {
-    ContentView()
+    StickManView()
 }
+
